ea773026178890fbcfa14588586a49e8a770d7ab
tiger121005 <itiger4649@gmail.com>
2023-03-23 15:43:46 +0900

    Initial Commit

---
+++ Karalog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Karalog
+//
+//  Created by 伊藤汰海 on 2023/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
