130301ca7d3c3740db62c22257ce8818e64b3006
tiger121005 <tiger121005@github.com\nalkvjn>
2024-08-28 21:49:31 +0900

    add home sort

---
+++ Karalog/View/AddMusicView.swift
@@ -0,0 +1,18 @@
+//
+//  AddMusicView.swift
+//  Karalog
+//
+//  Created by TAIGA ITO on 2024/08/09.
+//
+
+import SwiftUI
+
+struct AddMusicView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddMusicView()
+}
---
+++ Karalog/View/Custom/NormalTextField.swift
@@ -0,0 +1,18 @@
+//
+//  NormalTextField.swift
+//  Karalog
+//
+//  Created by TAIGA ITO on 2024/08/09.
+//
+
+import SwiftUI
+
+struct NormalTextField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NormalTextField()
+}
